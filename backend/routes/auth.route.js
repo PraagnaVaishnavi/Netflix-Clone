f3757342ef1cf3ad830cac63a5b4278f99a96f2a
PraagnaVaishnavi <147193570+PraagnaVaishnavi@users.noreply.github.com>
2025-04-06 12:35:26 +0530

    Initial commit

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { login,signup,logout } from "../controller/auth.controller.js"
+const router=express.Router();
+router.get('/signup',signup);
+router.get('/login',login);
+router.get('/logout',logout);
+export default router;
