d5793b4792718abe41377b9f767115a4c5bfd09c
PraagnaVaishnavi <147193570+PraagnaVaishnavi@users.noreply.github.com>
2025-04-08 19:10:22 +0530

    fetching movies and tv series API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import  {ENV_VARS}  from './config/envVars.js';
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 import { connectDB } from './config/db.js';
 const PORT=ENV_VARS.PORT;
 const app=express();
 
 app.use(express.json());
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/movie",movieRoutes);
+app.use("/api/v1/tv",tvRoutes);
 
-app.use("/api/v1/auth",authRoutes)
 
 app.listen(PORT,()=>{
 console.log("server started at http://localhost:"+ PORT);
