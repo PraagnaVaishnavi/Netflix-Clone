5f4b08be5ec7c2b31d8df90a7ff32ed119111fac
PraagnaVaishnavi <147193570+PraagnaVaishnavi@users.noreply.github.com>
2025-04-08 11:18:30 +0530

    jwt authentication

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 // File: backend/controller/auth.controller.js
 import User from "../models/user.model.js"
 import bcrypt from  "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generatetokens.js";
 export async function signup(req, res) {
     try {
       const {email,username,password}=req.body;
@@ -27,18 +28,57 @@ export async function signup(req, res) {
       const newUser =new User({
         email:email,password:hashedpassword,username:username,image:img
       })
+      generateTokenAndSetCookie(newUser._id,res);
       await newUser.save();
-      res.send("user logged in"+newUser.username);
+      res.status(201).json({
+        success:true,
+        user:{
+          ...newUser._doc,
+          password:"",
+        }
+      })
+
+      
     } catch (error) {
       return res.status(400).json({message:"error"});
     }
   }
   
   export async function login(req, res) {
-    res.send("login");
+   try {
+    const {email,password}=req.body;
+    console.log(req.body);
+    if (!email || !password) {
+      return res.status(400).json({ message: "Require all credentials" });
+    }
+      const user=await User.findOne({email:email});
+      if(!user){
+        return res.status(400).json({message:"invalid email"});
+      }
+    const ispwcorrect=  await bcrypt.compare(password,user.password);
+    if(!ispwcorrect){
+      return res.status(400).json({message:"invalid password"});
+    }
+    generateTokenAndSetCookie(user._id,res);
+    res.status(200).json({
+      success:true,
+      user:{
+        ...user._doc,
+        password:""
+      }
+    })
+   } catch (error) {
+    return res.status(400).json({message:error.message});
+   }
   }
   
   export async function logout(req, res) {
-    res.send("logout");
+    try {
+      res.clearCookie("jwt_netflix");
+      res.status(200).json({success:true,message:"logged out"});
+    } catch (error) {
+      console.log("error in logout controller",error.message);
+      return res.status(400).json({message:"error"});
+    }
   }
   
---
+++ backend/utils/generatetokens.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken"
+import { ENV_VARS } from "../config/envVars.js"
+export const generateTokenAndSetCookie=(userId,res)=>{
+    const token=jwt.sign({userId},ENV_VARS.JWT_SECRET,{expiresIn:"15d"});
+    res.cookie("jwt-netflix",token,{
+     maxAge:15*60*60*24*1000, httpOnly:true,
+     sameSite:"strict",
+     secure:ENV_VARS.NODE_ENV!=="development",
+    });
+    return token;
+}
