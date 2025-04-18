25834447758de1e13a8bde6fe2617099aa1dd5bc
PraagnaVaishnavi <147193570+PraagnaVaishnavi@users.noreply.github.com>
2025-04-18 15:05:11 +0530

    404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,8 @@ import { useAuthStore } from './store/authUser.js'
 import { Loader } from "lucide-react";
 import WatchPage from './pages/WatchPage.jsx'
 import SearchPage from './pages/SearchPage.jsx'
-
+import SearchHistoryPage from './pages/SearchHistoryPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
   useEffect(() => {
@@ -34,6 +35,8 @@ function App() {
     <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
     <Route path='/watch/:id' element={user?<WatchPage/>:<Navigate to={"/login"}/>}/>
     <Route path='/search' element={user?<SearchPage/>:<Navigate to={"/login"}/>}/>
+    <Route path='/history' element={user?<SearchHistoryPage/>:<Navigate to={"/login"}/>}/>
+    <Route path='/*' element={<NotFoundPage/>}/>
    </Routes>
      <Footer/>
      <Toaster/>
