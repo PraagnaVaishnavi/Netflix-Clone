2a3e000b937d244d4fb5a2fe4d348ec7e236111a
Praagna L Vaishnavi <147193570+PraagnaVaishnavi@users.noreply.github.com>
2025-04-29 23:07:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://netflix-clone-2-backend.onrender.com",
         changeOrigin: true,
         secure: false
       }
